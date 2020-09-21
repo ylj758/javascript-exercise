5a9137c5397dcb26b643bc08c0f966b4be5bec31
ylj <758766951@qq.com>
2020-09-21 19:01:21 +0800

    [ylj]: implement 3-object

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,12 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const newObject = {
+    serialNumber: '12345',
+    type: 'accessory',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  return Object.assign(source, newObject);
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(ele => Number(ele))
+    .reduce((x, y) => x + y);
 }
