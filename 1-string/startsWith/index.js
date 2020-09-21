f18caee4eba1097d9b96841842c1fe455dc31380
ylj <758766951@qq.com>
2020-09-21 15:35:48 +0800

    [ylj]: implement 1-string

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const collectionFilter = collection.filter(str => str.startsWith('粤A'));
+  return collectionFilter.length;
 }
--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,7 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const score = getScore(person.id);
+  const result = '你好，';
+  return result.concat(person.lastName, person.firstName, '！你的考试成绩为', score, '分');
 }
